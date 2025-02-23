8222888a676e80198ac7e09c2123178a49c5e293
whitegive553 <2480118796@qq.com>
2025-02-23 03:19:13 -0500

    try

--- card-game-client/src/router.js
+++ card-game-client/src/router.js
@@ -6,9 +6,15 @@ import Login from './pages/Login.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'hash',   // 确保使用 hash 模式
   routes: [
     {
       path: '/',
+      name: 'home',
+      redirect: '/gameTable'   // 根路径重定向到 gameTable
+    },
+    {
+      path: '/gameTable',
       name: 'gameTable',
       component: GameTable
     },
